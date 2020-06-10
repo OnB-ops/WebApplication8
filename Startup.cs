f3b7ef416876afe610f0e9df03c8424b7cffe282
burdukov <Polygo@yandex.ru>
2020-06-11 01:30:57 +0700

    V4

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using WebApplication8.Controllers;
+using WebApplication8.Models;
+using WebApplication8.Storage;
 
 namespace WebApplication8
 {
-    public class Class
+    public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+           services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            switch (Configuration["Storage:Type"].ToStorageEnum())
+            {
+                case StorageEnum.MemCache:
+                    services.AddSingleton<IStorage<Lab1Data>, MemCache>();
+                    break;
+                case StorageEnum.FileStorage:
+                    services.AddSingleton<IStorage<Lab1Data>>(
+                        x => new FileStorage(Configuration["Storage:FileStorage:Filename"], int.Parse(Configuration["Storage:FileStorage:FlushPeriod"])));
+                    break;
+                default:
+                   throw new IndexOutOfRangeException($"Storage type '{Configuration["Storage:Type"]}' is unknown");
+
+            }
+
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices1(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
